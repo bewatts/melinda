f85a1caa464f8e7866e36491b36e29cf17e916af
justalisteningman <stoikal@gmail.com>
2013-11-19 10:01:15 -0800

    initial commit, nothing done

---
+++ specs/unit_spec.js
@@ -0,0 +1,14 @@
+function(root){
+  var Melinda = root.Melinda = (root.Melinda || {})
+}(this)
+
+window.onload = function(){
+  test("TestName", function(){
+    pause();
+    setTimeout(function())
+  });
+  
+  
+  
+  
+};
